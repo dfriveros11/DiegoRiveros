f2848b54522fd8ceb8b255bb59313e77860ec51e
Diego Riveros <df.riveros11@uniandes.edu.co>
2020-02-01 21:55:28 -0500

    almost there

--- js/script.js
+++ js/script.js
@@ -41,3 +41,15 @@ function updateCardPositions() {
 
   }, 2000);
 }
+
+
+var $grid = $('.gallery-wrapper').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+    transitionDuration: 0,
+  });
+
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });
